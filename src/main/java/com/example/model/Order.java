eb38c4c1ae55fcc397ae1f0fb5cf1cb02fe8c96a
Janith Thilakarathna <janith.thilakaratna@gmail.com>
2021-08-20 23:52:50 +0530

    added order model

---
+++ src/main/java/com/example/model/Order.java
@@ -0,0 +1,18 @@
+package com.example.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+}
